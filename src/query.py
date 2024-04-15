0126736b1962c18b9d8f9aeed711723d6f813e50
Dev M Chechi <devmchechi@gmail.com>
2024-04-15 10:38:48 -0400

    Fix Paths

--- src/query.py
+++ src/query.py
@@ -115,7 +115,7 @@ def create_sample_database(catalog_name):
     connection = duckdb.connect(test_db_path, False, "vaultdb")
     
     connection.execute(f"CREATE CONFIG remote AS 's3://{data_store}/{catalog_name}';")
-    connection.execute(f"CREATE CONFIG remote_merge_path AS 's3://{public_bucket}/{catalog_name}';")
+    connection.execute(f"CREATE CONFIG remote_merge_path AS 's3://{public_bucket}';")
 
     connection.execute(f"CREATE CONFIG application_name AS '{application_name}';")
     user_pool_client_id = os.environ['user_pool_client_id'] if "user_pool_client_id" in os.environ else None
